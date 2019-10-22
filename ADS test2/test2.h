7ffa3e81f5c4934adc0ac3099a17021d50fdd9ce
sahanaKS-033 <55292137+sahanaKS-033@users.noreply.github.com>
2019-10-22 16:14:14 +0530

    Add files via upload

---
+++ ADS test2/test2.h
@@ -0,0 +1,23 @@
+#ifndef _INCLUDED_HEAP_
+#define _INCLUDED_HEAP_
+#include <time.h>
+
+#include <stdint.h>
+#define HEAP_MAX_SIZE 32
+
+typedef struct _heap_ Heap;
+struct  _heap_
+{
+	uint32_t	size;
+	int32_t		data[HEAP_MAX_SIZE]	;
+};
+
+Heap 		heap_new(int32_t data[], uint32_t len);
+Heap*		heap_sort(Heap *heap);
+Heap*		heap_insert(Heap *heap, int32_t key);
+Heap*		heap_test(Heap *heap);
+int32_t		heap_get_max(Heap *heap);
+int32_t		heap_extract_max(Heap *heap);
+uint32_t	heap_size(Heap *heap);
+
+#endif
